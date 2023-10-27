6491f0ee923ecf35e462577d7048e055f672bb33
Jaydon Yancey <jaydonyancey7@gmail.com>
2023-10-27 16:18:55 -0600

    added additional meta data

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Customers Page Example',
+};
+
 
 export default function Customers() {
   return (
