209229954a6d46d404645c30f34d47009236511e
Jaydon Yancey <jaydonyancey7@gmail.com>
2023-10-27 21:50:42 -0600

    fixes

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,19 +1,19 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
-import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
-import { InvoiceForm } from "@/app/lib/definitions";
+import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Update Invoice',
+  title: "Edit Invoice",
 };
-
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
-  const invoice = await fetchInvoiceById(id);
-  const customers = await fetchCustomers();
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
 
   if (!invoice) {
     notFound();
@@ -31,7 +31,7 @@ export default async function Page({ params }: { params: { id: string } }) {
           },
         ]}
       />
-      <Form invoice={invoice as InvoiceForm} customers={customers} />
+      <Form invoice={invoice} customers={customers} />
     </main>
   );
 }
